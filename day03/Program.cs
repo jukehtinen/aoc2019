1ab7d0a91754e2a863bff6ec2d3dd4d9a0e31fa7
jukehtinen <jukehtinen@gmail.com>
2019-12-04 10:36:35 +0200

    Day 3 and 4 solutions

---
+++ day03/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace day03
+{
+    struct Point
+    {
+        public int X;
+        public int Y;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("input.txt");
+
+            // Part 1
+            var n1 = GetNodes(lines[0]);
+            var n2 = GetNodes(lines[1]);
+            n1.IntersectWith(n2);
+            Console.WriteLine(n1.Min(n => Math.Abs(n.Item1) + Math.Abs(n.Item2)));
+
+            // Part 2
+            var n3 = GetNodes2(lines[0]);
+            var n4 = GetNodes2(lines[1]);
+            var intersect = n3.Keys.Intersect(n4.Keys).ToDictionary(t => t, t => n3[t] + n4[t]);
+            Console.WriteLine(intersect.Min(v => v.Value));
+        }
+
+        private static HashSet<Tuple<int, int>> GetNodes(string line)
+        {
+            var set = new HashSet<Tuple<int, int>>();
+            var dir = new Point();
+            var current = new Point();
+            foreach (var t in line.Split(','))
+            {
+                var val = int.Parse(t.Substring(1));
+                if (t[0] == 'L') { dir.X = -1; dir.Y = 0; }
+                if (t[0] == 'R') { dir.X = 1; dir.Y = 0; }
+                if (t[0] == 'U') { dir.X = 0; dir.Y = -1; }
+                if (t[0] == 'D') { dir.X = 0; dir.Y = 1; }
+
+                for (var i = 0; i < val; i++)
+                {
+                    current.X += dir.X;
+                    current.Y += dir.Y;
+                    set.Add(new Tuple<int, int>(current.X, current.Y));
+                }
+            }
+
+            return set;
+        }
+
+        private static Dictionary<Tuple<int, int>, int> GetNodes2(string line)
+        {
+            var set = new Dictionary<Tuple<int, int>, int>();
+            var dir = new Point();
+            var current = new Point();
+            var steps = 0;
+            foreach (var t in line.Split(','))
+            {
+                var val = int.Parse(t.Substring(1));
+                if (t[0] == 'L') { dir.X = -1; dir.Y = 0; }
+                if (t[0] == 'R') { dir.X = 1; dir.Y = 0; }
+                if (t[0] == 'U') { dir.X = 0; dir.Y = -1; }
+                if (t[0] == 'D') { dir.X = 0; dir.Y = 1; }
+
+                for (var i = 0; i < val; i++)
+                {
+                    current.X += dir.X;
+                    current.Y += dir.Y;
+                    set.TryAdd(new Tuple<int, int>(current.X, current.Y), ++steps);
+                }
+            }
+
+            return set;
+        }
+    }
+}
---
+++ day04/day04.cpp
@@ -0,0 +1,82 @@
+﻿#include <charconv>
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::string input = "138241-674034";
+
+	auto l1 = std::atoi(input.substr(0, input.find_first_of('-')).c_str());
+	auto l2 = std::atoi(input.substr(input.find_first_of('-') + 1).c_str());
+
+	static auto toDigit = [](const std::string& input, int index) -> int {
+		auto val = 0;
+		std::from_chars(input.data() + index, input.data() + index + 1, val);
+		return val;
+	};
+
+	// Two adjacent digits are the same
+	auto adjacentDigits = [](int value) -> bool {
+		auto v = std::to_string(value);
+		for (auto j = 0; j < v.size() - 1; j++)
+		{
+			if (toDigit(v, j) == toDigit(v, j + 1))
+				return true;
+		}
+		return false;
+	};
+
+	// two adjacent matching digits are not part of a larger group of matching digits
+	auto adjacentDigitsPart2 = [](int value) -> bool {
+		auto v = std::to_string(value);
+		auto group = toDigit(v, 0);
+		auto groupSize = 1;
+		for (auto j = 1; j < v.size(); j++)
+		{
+			if (toDigit(v, j) == group)
+			{
+				groupSize++;
+			}
+			else
+			{
+				if (groupSize == 2)
+					return true;
+				groupSize = 1;
+			}
+			group = toDigit(v, j);
+		}
+		return groupSize == 2;
+	};
+
+	// Going from left to right, the digits never decrease
+	auto digitsIncrease = [](int value) -> bool {
+		auto v = std::to_string(value);
+		auto n = toDigit(v, 0);
+		for (auto j = 1; j < v.size(); j++)
+		{
+			auto n2 = toDigit(v, j);
+			if (n2 < n)
+				return false;
+			n = n2;
+		}
+		return true;
+	};
+
+	int count = 0;
+	for (auto i = l1; i < l2; i++)
+	{
+		if (adjacentDigits(i) && digitsIncrease(i))
+			count++;
+	}
+	std::cout << "Part 1: " << count << "\n";
+
+	count = 0;
+	for (auto i = l1; i < l2; i++)
+	{
+		if (adjacentDigits(i) && digitsIncrease(i) && adjacentDigitsPart2(i))
+			count++;
+	}
+	std::cout << "Part 2: " << count << "\n";
+
+	return 0;
+}
